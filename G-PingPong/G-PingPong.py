1ad3049b2073f6f150b400c08f87500e1666dd9d
Lande <64076502+Laande@users.noreply.github.com>
2023-01-12 23:19:50 +0000

    refractor, modification

--- G-PingPong/G-PingPong.py
+++ G-PingPong/G-PingPong.py
@@ -1,6 +1,7 @@
 import sys
 from g_python.gextension import Extension
 from g_python.hmessage import Direction
+from g_python.hpacket import HPacket
 
 extension_info = {
     "title": "G-PingPong",
@@ -29,184 +30,143 @@ START_CMD = ":pstart"
 STOP_CMD = ":pstop"
 ##################################
 
-furniclick = False
-setup = False
-on = False
-left = None          # The side you playing on
+furniclick = in_setup = on = False
+left = None                      # The side you playing on
 
-left_chair = []
-left_tile = []
-right_chair = []
-right_tile = []
-ball_id = 0          # Ball's Furni ID
-left_ball = []       # Ball on left tile
-right_ball = []      # Ball on right tile
+chair = tile = ball = [[], []]   # Coord for both side, x[0] left, x[1] right
+ball_id = 0                      # Ball's Furni ID
 
 
-def walk(x,y):       # Makes the user walk to coords
-    ext.send_to_server('{h:'+str(USER_MOVE)+'}{i:'+str(x)+'}{i:'+str(y)+'}')
-    
+def walk(x, y):       # Makes the user walk to coords
+    ext.send_to_server(HPacket(USER_MOVE, int(x), int(y)))
+
+
 def chat(message):   # Sends client messages
-    ext.send_to_client('{h:'+str(SPEECH_IN)+'}{i:0}{s:"'+str(message)+'"}{i:0}{i:1}{i:0}{i:0}')
-                       
-def speech(message): # User used the command
-    global setup
-    global left
-    global on
-    global left_chair
-    global left_tile
-    global right_chair
-    global right_tile
-    global ball_id
-    global left_ball
-    global right_ball
-    
-    (text,_,_) = message.packet.read('sii')
-    if text == HELP_CMD:
-        message.is_blocked = True
-        chat(''+SETUP_CMD+': Starts the tile, ball setup')
-        chat(''+START_CMD+': Starts the g-pingpong')
-        chat(''+STOP_CMD+': Stops the g-pingpong')
-        chat(''+LEFT_CMD+': Use if you are sitting on the left side.')
-        chat(''+RIGHT_CMD+': Use if you are sitting on the right side.')
-        return
+    ext.send_to_client(HPacket(SPEECH_IN, 0, str(message), 0, 1, 0, 0))
 
-    if text == LEFT_CMD:
-        message.is_blocked = True
+def reset():
+    global chair, tile, ball, ball_id
+
+    chair = tile = ball = [[], []]
+    ball_id = 0
+
+
+def speech(p): # User used the command
+    global in_setup, left, on
+
+    text, _, _ = p.packet.read('sii')
+
+    if text == HELP_CMD:
+        p.is_blocked = True
+        chat(f'''
+            {SETUP_CMD} : Starts the tile, ball setup \n
+            {START_CMD} : Starts the g-pingpong \n
+            {STOP_CMD} : Stops the g-pingpong \n
+            {LEFT_CMD} : Use if you are sitting on the left side. \n
+            {RIGHT_CMD} : Use if you are sitting on the right side.
+        ''')
+
+    elif text == LEFT_CMD:
+        p.is_blocked = True
         left = True
         chat('Left chair selected!')
-        return
 
-    if text == RIGHT_CMD:
-        message.is_blocked = True
+    elif text == RIGHT_CMD:
+        p.is_blocked = True
         left = False
         chat('Right chair selected!')
-        return
 
-    if text == SETUP_CMD:
-        message.is_blocked = True
-        setup = True
-        left_chair = []
-        left_tile = []
-        right_chair = []
-        right_tile = []
-        ball_id = 0
-        left_ball = []
-        right_ball = []
+    elif text == SETUP_CMD:
+        p.is_blocked = True
+        reset()
+        in_setup = True
         chat('Please click the left chair.')
-        return
 
-    if text == START_CMD:
-        message.is_blocked = True
-        if not right_ball:
-            chat('Please do the setup first! - '+SETUP_CMD+'')
-            return
-        if left == None:
-            chat('Please select a side first! - '+LEFT_CMD+' , '+RIGHT_CMD+'')
-            return
-        on = True
-        chat('G-PingPong started!')
-        return
+    elif text == START_CMD:
+        p.is_blocked = True
 
-    if text == STOP_CMD:
-        message.is_blocked = True
+        if not ball:
+            chat(f'Please do the setup first! - {SETUP_CMD}')
+        elif left is None:
+            chat(f'Please select a side first! - {LEFT_CMD} , {RIGHT_CMD}')
+        else:
+            on = True
+            chat('G-PingPong started!')
+
+    elif text == STOP_CMD:
+        p.is_blocked = True
         on = False
         chat('G-PingPong stopped!')
-        return
-    
-def setup(message): # User clicks to choose coords
-    global setup
-    global furniclick
-    
-    if setup:
-        (x,y) = message.packet.read('ii')
-        if not left_chair:
-            left_chair.append(x)
-            left_chair.append(y)
-            chat(left_chair)
+
+
+def setup(p): # User clicks to choose coords
+    global in_setup, furniclick
+
+    if in_setup:
+        x, y = p.packet.read('ii')
+
+        if not chair[0]:
+            chair[0] = [x, y]
+            chat(f"Left chair : {chair[0]}")
             chat('Please click the left tile.')
-            return
 
-        if not left_tile:
-            left_tile.append(x)
-            left_tile.append(y)
-            chat(left_tile)
+        elif not tile[0]:
+            tile[0] = [x, y]
+            chat(f"Left tile : {tile[0]}")
             chat('Please click the right chair.')
-            return
 
-        if not right_chair:
-            right_chair.append(x)
-            right_chair.append(y)
-            chat(right_chair)
+        elif not chair[1]:
+            chair[1] = [x, y]
+            chat(f"Right chair : {chair[1]}")
             chat('Please click the right tile.')
-            return
 
-        if not right_tile:
-            right_tile.append(x)
-            right_tile.append(y)
-            chat(right_tile)
+        elif not tile[1]:
+            tile[1] = [x, y]
+            chat(f"Right chair : {tile[1]}")
             chat('Please click the left ball tile.')
-            return
 
-        if not left_ball:
-            left_ball.append(x)
-            left_ball.append(y)
-            chat(left_ball)
+        elif not ball[0]:
+            ball[0] = [x, y]
+            chat(f"Left ball : {ball[0]}")
             chat('Please click the right ball tile.')
-            return
 
-        if not right_ball:
-            right_ball.append(x)
-            right_ball.append(y)
-            chat(right_ball)
+        elif not ball[1]:
+            ball[1] = [x, y]
+            chat(f"Right ball : {ball[1]}")
             chat('Please double-click the ball.')
             furniclick = True
-            setup = False
-            return
-        
-def set_ball(message): # Find ball's ID
-    global furniclick
-    global ball_id
-    
+            in_setup = False
+
+
+def set_ball(p): # Find ball's ID
+    global furniclick, ball_id
+
     if furniclick:
-        (furni_id,_) = message.packet.read('ii')
-        ball_id = furni_id
+        ball_id, _ = p.packet.read('ii')
         furniclick = False
-        chat(str(ball_id))
-        chat('Setup Complete! Type '+LEFT_CMD+' or '+RIGHT_CMD+' before start.')
+        chat(f"{ball_id=}")
+        chat(f'Setup Complete! \n Type {LEFT_CMD} or {RIGHT_CMD} before start.')
+
+
+def pingpong(p):
+    _, _, x, y, _, furni, _, _, _ = p.packet.read('iiiiiissi')
+
+    if not on or ball_id != furni:
         return
 
-def pingpong(message):
-    global left_chair
-    global left_tile
-    global right_chair
-    global right_tile
-    global ball_id
-    global left_ball
-    global right_ball
-    global left
-    
-    if on:
-        (_,_,x,y,_,ball,_,_,_) = message.packet.read('iiiiiissi')
-        if ball_id == ball:
-            if left:
-                if x and y in left_ball:
-                    walk(left_tile[0],left_tile[1])
-                    return
-                elif x and y in right_ball:
-                    walk(left_chair[0],left_chair[1])
-                    return
-            elif not left:
-                if x and y in left_ball:
-                    walk(right_chair[0],right_chair[1])
-                    return
-                elif x and y in right_ball:
-                    walk(right_tile[0],right_tile[1])
-                    return
-            
-#######################################################################
+    if left:
+        if ball[0] == [x, y]:
+            walk(tile[0], tile[1])
+        elif ball[1] == [x, y]:
+            walk(chair[0], chair[1])
+    else:
+        if ball[0] == [x, y]:
+            walk(chair[0], chair[1])
+        elif ball[1] == [x, y]:
+            walk(tile[0], tile[1])
+
+
 ext.intercept(Direction.TO_SERVER, speech, SPEECH_OUT)
 ext.intercept(Direction.TO_SERVER, setup, USER_MOVE)
 ext.intercept(Direction.TO_SERVER, set_ball, USE_FURNI)
 ext.intercept(Direction.TO_CLIENT, pingpong, WIRED_MOVE_FURNI)
-#######################################################################
